2db477f88a085c3eb494bf8f764fa83e78803cb6
marios292 <mario.silgado@educa.madrid.org>
2024-11-26 14:21:04 +0100

    a

--- java/ejemplo1.1.js
+++ java/ejemplo1.1.js
@@ -8,3 +8,9 @@ document.write("<table border=\"1\">");
 document.write("<tr><th>Nombre</th><th>Instituto</th></tr>");
 document.write("<tr><td>" + nombre + "</td><td>" + instituto + "</td></tr>");
 document.write("</table");
+
+
+var nota1 = 5;
+var nota2 = 4;
+var notaTotal = nota1 + nota2;
+var notaMedia = (nota1 + nota2) / 2;
