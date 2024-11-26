c2ff026bb6508916f86c05b02cd017a12efe06cd
marios292 <mario.silgado@educa.madrid.org>
2024-11-26 14:08:54 +0100

    a

--- java/ejemplo1.1.js
+++ java/ejemplo1.1.js
@@ -1,1 +1,8 @@
-prompt("Dime tu nombre");
+var nombre = prompt("Dime tu nombre");
+var instituto = prompt("Nombre de instituto");
+console.log(nombre);
+//alert(nombre);
+document.write(`<table border=\"1\">`)
+document.write(`<tr><td><th>Nombre</th><th>Instituto</th></tr>`);
+document.write("<tr><td>" + nombre + "</td><td>" + instituto + "</td></tr>")
+document.write("</table")
