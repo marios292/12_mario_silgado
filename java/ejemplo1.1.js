d10a59d00d56a5c0dab098bcf9f80b0e14d80677
marios292 <mario.silgado@educa.madrid.org>
2024-11-26 13:45:11 +0100

    a

---
+++ java/ejemplo1.1.js
@@ -0,0 +1,1 @@
+prompt("Dime tu nombre");
--- java/p1.js
+++ java/p1.js
@@ -1,3 +1,25 @@
 console.log('prueba de código');
 
-function imprimir
+function imprimir(mensaje) {
+    console.log(mensaje);
+
+    console.log("Hola mundo");
+}
+
+function tipoDeVariable(minivariable) {
+    console.log("Mi variable es de tipo" + typeof (minivariable));
+}
+
+
+
+var alumnosFila2 = ["Jacob", "Aleks", "David", "Jorge"];
+console.log(alumnosFila2);
+
+var notaExamenJs = 6;
+tipoDeVariable(notaExamenJs);
+
+var miNombre = "Mario";
+tipoDeVariable(miNombre);
+
+
+
