9dc389a9e02023fa706c2244ecd4fa75b3aff86b
marios292 <mario.silgado@educa.madrid.org>
2024-11-28 12:20:22 +0100

    a

--- java/ejemplo1.1.js
+++ java/ejemplo1.1.js
@@ -3,7 +3,7 @@
 function mostrarFecha() {
     var now = Date();
     console.log("Ahora son: " + now)
-    alert("Ahora son las: " + now)
+    //alert("Ahora son las: " + now)
     document.write("<h1>" + now + "</h1>")
 
 }
